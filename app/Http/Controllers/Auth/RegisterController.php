3615b3ddc6719a709b093dbd19c8f17f7cd40ccf
yuuki0114 <y.no0114@gmail.com>
2020-11-25 07:23:05 +0900

    プロフィール画像を登録できるようにする

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,9 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Facades\Image;
+use App\Services\CheckExtensionServices;
+use App\Services\FileUploadServices;
 
 class RegisterController extends Controller
 {
@@ -53,6 +56,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'img_name' => ['file', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2000'],
+            'self_introduction' => ['string', 'max:255'],
         ]);
     }
 
@@ -64,13 +69,30 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        //引数 $data から name='img_name'を取得
+        $imageFile = $data['img_name'];
+
+        //$imageFileからファイル名を取得
+        $list = FileUploadServices::fileUpload($imageFile);
+
+        list($extension, $fileNameToStore, $fileData) = $list;
+
+        //拡張子ごとに base64エンコード実施
+        $data_url = CheckExtensionServices::checkExtension($fileData, $extension); 
+
+        //画像アップロード
+        $image = Image::make($data_url);
+
+        //画像を横400px, 縦400pxにリサイズし保存
+        $image->resize(400,400)->save(storage_path() . '/app/public/images/' . $fileNameToStore );
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'self_introduction' => $data['self_introduction'],
             'sex' => $data['sex'],
-            'img_name' => $data['img_name'],
+            'img_name' => $fileNameToStore,
         ]);
     }
 }
